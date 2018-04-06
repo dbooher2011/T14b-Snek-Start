9c92697feb75df77cf77e6f96339dad34e6ed133
David Booher <dbooher2011@gmail.com>
2018-04-06 01:00:57 -0400

    Add project files.

---
+++ Engine/Snake.h
@@ -0,0 +1,39 @@
+#pragma once
+
+#include "Board.h"
+
+class Snake
+{
+private:
+	class Segment
+	{
+	public:
+		void InitHead(const Location& in_loc);
+		void InitBody(const Color c_in);
+		void Follow(const Segment& next);
+		void MoveBy(const Location& delta_loc);
+		void Draw(Board& brd) const;
+		const Location GetLocation() const;
+	private:
+		Location loc;
+		Color c;
+	};
+
+public:
+	Snake(const Location& loc);
+	void MoveBy(const Location& delta_loc);
+	Location GetNextHeadLocation(const Location& delta_loc) const;
+	void Grow();
+	void Draw(Board& brd);
+	bool IsInTileExceptEnd(const Location& target) const;
+	bool IsInTile(const Location& target) const;
+
+private:
+	static constexpr Color headColor = Colors::Yellow;
+
+	static constexpr Color bodyColorOdd = { 159, 255, 128 };
+	static constexpr Color bodyColorEven = { 45, 179, 0 };
+	static constexpr int nSegmentsMax = 100;
+	Segment segments[nSegmentsMax];
+	int nSegments = 1;
+};
